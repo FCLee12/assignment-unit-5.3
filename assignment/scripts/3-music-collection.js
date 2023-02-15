db1ba3f3dcbc874fe98d3e51eaa04462891f83da
Fue Lee <fueclee.12@gmail.com>
2023-02-15 13:52:16 -0600

    completed 1st iteration of addToCollection function, doing more testing

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,18 @@
 console.log('***** Music Collection *****')
 let collection = [];
 
+function addToCollection( title, artist, yearPublished ) {
+    let album = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    };
+    console.log( 'Adding this album to the collection:', album );
+    collection.push( album );
+    return album
+}
+
+// *****addToCollection Testing*****
+console.log( '*****addToCollection Testing*****' );
+
+console.log( addToCollection( 'Song', 'Musician', '2000' ) );
