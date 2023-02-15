8e0fc7364e97a641664404f4482392b6776313f8
Fue Lee <fueclee.12@gmail.com>
2023-02-15 14:40:56 -0600

    completed showCollection function and testing for the showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -12,14 +12,28 @@ function addToCollection( title, artist, yearPublished ) {
     return album
 }
 
+function showCollection( array ) {
+    console.log( 'There are ' + array.length + ' albums in this collection');
+    for( let item of array ) {
+        console.log( item.title + ' by ' + item.artist + ', published in ' + item.yearPublished );
+    }
+}
+
+//*****Testing Zone*****
+
 // *****addToCollection Testing*****
 console.log( '*****addToCollection Testing*****' );
 
-console.log( addToCollection( 'Song', 'Musician', '2000' ) );
-console.log( addToCollection( 'Song1', 'Musician1', '2001' ) );
-console.log( addToCollection( 'Song2', 'Musician2', '2002' ) );
-console.log( addToCollection( 'Song3', 'Musician3', '2003' ) );
-console.log( addToCollection( 'Song4', 'Musician4', '2004' ) );
-console.log( addToCollection( 'Song5', 'Musician5', '2005' ) );
+console.log( addToCollection( 'Album', 'Musician', '2000' ) );
+console.log( addToCollection( 'Album1', 'Musician1', '2001' ) );
+console.log( addToCollection( 'Album2', 'Musician2', '2002' ) );
+console.log( addToCollection( 'Album3', 'Musician3', '2003' ) );
+console.log( addToCollection( 'Album4', 'Musician4', '2004' ) );
+console.log( addToCollection( 'Album5', 'Musician5', '2005' ) );
 
 console.log( collection );
+
+// *****showCollection Testing*****
+console.log( '*****showCollection Testing*****' );
+
+showCollection( collection );
